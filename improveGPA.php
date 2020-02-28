a187b79a95973aa72972066ad5a5e864bcb084d2
Andrew Mehta <andmehta@icloud.com>
2020-02-28 14:13:19 -0600

    moved _POST variables to outside of checks, and then used a basic null check to determine if the page has been visited

--- improveGPA.php
+++ improveGPA.php
@@ -15,44 +15,49 @@
     <?php
       // initialize necessary variables fix
       $nameErr = $emailErr = $agreeErr = $gpaErr = $creditsErr = $newErr = $increaseErr = "";
-      $name = $agree = $credits = $new = $increase = "";
-      $gpa = 1;
+      $name = $_POST['name'];
+      $email = isset($_POST['email']) ? $_POST['email'] : "aaa@aaa.aaa";
+      $agree = isset($_POST['agree']) ? $_POST['agree'] : "True";
+      $currentGPA = $_POST["currentGPA"];
+      $currentCredits = $_POST["currentCredits"];
+      $newCredits = isset($_POST['newCredits']) ? $_POST['newCredits'] : 0;
+      $GPAincrease = $_POST["GPAincrease"];
 
       // regex gladly taken from https://www.w3schools.com/php/php_form_url_email.asp
       // issues here. doesn't trigger error
-      if   (!preg_match("/^[a-zA-Z ]*$/",$_POST['name'])) {
+      if   (!preg_match("/^[a-zA-Z ]*$/",$name)) {
         $nameErr = "Your name must consist of letters and white space";
       }
 
       // Checks the email to validate it is a proper email
       // non-functional. doesn't err when improper email is inputted
-      if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+      if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         $emailErr = "Invalid email format";
       }
 
       // This needs to not trip on original view of the site.
-      if (!isset($_POST['agree']) ) {
+      if (!isset($agree) ) {
         $agreeErr = "You must agree to the terms and conditions";
       }
 
       // ensure that the inputted GPA falls between 0 and 4.0 inclusive
-      if (floatval($_POST["currentGPA"]) < 0 || floatval($_POST["currentGPA"]) > 4.0) {
+      if (floatval($currentGPA) < 0 || floatval($currentGPA) > 4.0) {
           $gpaErr = "Your current GPA must be a number between 0.0 and 4.0";
       }
 
       // current credits must be an integer and greater than or equal to 0. Needs an extra or statement for the edge case 0
       // found on https://www.w3schools.com/php/filter_validate_int.asp
-      if ($_POST["currentCredits"] < 0 || (!$_POST["currentCredits"] == 0 && !filter_var($_POST["currentCredits"], FILTER_VALIDATE_INT))) {
+      if ($currentCredits < 0 || (!$currentCredits == 0 && !filter_var($currentCredits, FILTER_VALIDATE_INT))) {
         $creditsErr = "Your current number of credits must be a positive integer";
       }
 
       // You have to take more than 0 credits a semester
       // triggers on original load. need a fix
-      if ($_POST["newCredits"] < 0 || !filter_var($_POST["newCredits"], FILTER_VALIDATE_INT)) {
+      if ($newCredits < 0 || !filter_var($newCredits, FILTER_VALIDATE_INT)) {
         $newErr = "(the number of credits this semester an integer greater than 0)";
       }
 
-      if ($_POST["GPAincrease"] < 0) {
+      if ($GPAincrease < 0) {
         $increaseErr = "(your desired GPA increase must be a positive value)";
       }
 
